5ef8105eb35949be6f59fea63c44678e913e0eef
jsdev-robin <jsdev.robin@gmail.com>
2025-12-15 21:44:03 +0600

    add app

--- apps/web/src/app/(panel)/account/dashboard/layout.tsx
+++
@@ -1,5 +0,0 @@
-const DashboardLayout = async ({ children }: { children: React.ReactNode }) => {
-  return <main className="p-4 space-y-4">{children}</main>;
-};
-
-export default DashboardLayout;
--- apps/web/src/store/useUser.ts
+++
@@ -1,9 +0,0 @@
-import { RootState } from "@/libs/features/store";
-import { useSelector } from "react-redux";
-
-const useUser = () => {
-  const user = useSelector((store: RootState) => store.auth.user);
-  return user;
-};
-
-export default useUser;
