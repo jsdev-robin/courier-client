c5dfaf4cc3f2afd05d4da353b8f8126aa6b5675a
jsdev-robin <jsdev.robin@gmail.com>
2025-12-16 12:34:45 +0600

    feat: obj2Form

--- apps/web/src/components/site/panel/dashboard/pages/settings/profile/Profile.tsx
+++ apps/web/src/components/site/panel/dashboard/pages/settings/profile/Profile.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { useUpdateProfileMutation } from '@/libs/features/services/auth/authApi';
 import useUser from '@/store/useUser';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Button } from '@repo/ui/components/button';
@@ -7,16 +8,23 @@ import { Form } from '@repo/ui/components/form';
 import Heading from '@repo/ui/components/heading';
 import { Spinner } from '@repo/ui/components/spinner';
 import Typography from '@repo/ui/components/typography';
+import {
+  DEFAULT_SERVER_ERROR_MESSAGE,
+  DEFAULT_SUCCESS_MESSAGE,
+} from '@repo/ui/utils/contants';
+import { obj2Form } from '@repo/ui/utils/obj2Form';
 import { authSchema } from '@repo/ui/validations/authSchema';
 import { Edit } from 'lucide-react';
 import { useState } from 'react';
 import { useForm } from 'react-hook-form';
+import { toast } from 'sonner';
 import { z } from 'zod';
 import ProfileCoordsMap from './particles/ProfileCoordsMap';
 import ProfileHeaderCard from './particles/ProfileHeaderCard';
 import ProfilePersonalInfoCard from './particles/ProfilePersonalInfoCard';
 
 const Profile = () => {
+  const [updateProfile, { isLoading }] = useUpdateProfileMutation();
   const user = useUser();
   const [isEditing, setIsEditing] = useState<boolean>(false);
   const form = useForm<z.infer<typeof authSchema.updateProfile>>({
@@ -40,10 +48,16 @@ const Profile = () => {
   });
 
   async function onSubmit(data: z.infer<typeof authSchema.updateProfile>) {
-    console.log(data);
+    await toast.promise(updateProfile(obj2Form(data)).unwrap(), {
+      loading: 'Updating your profile...',
+      success: (res) => {
+        setIsEditing(false);
+        form.reset();
+        return res?.message || DEFAULT_SUCCESS_MESSAGE;
+      },
+      error: (err) => err?.data?.message || DEFAULT_SERVER_ERROR_MESSAGE,
+    });
   }
-
-  const isLoading = false;
 
   return (
     <section>
--- packages/ui/src/utils/obj2Form.ts
+++ packages/ui/src/utils/obj2Form.ts
@@ -1,4 +1,4 @@
-export function formatFormData<T extends object>(
+export function obj2Form<T extends object>(
   obj: T,
   form?: FormData,
   parentKey?: string,
@@ -12,21 +12,21 @@ export function formatFormData<T extends object>(
     const formKey = parentKey ? `${parentKey}[${key}]` : key;
 
     if (value === null || value === undefined) {
-      formData.append(formKey, "");
+      formData.append(formKey, '');
     } else if (value instanceof File || value instanceof Blob) {
       formData.append(formKey, value);
     } else if (value instanceof Date) {
       formData.append(formKey, value.toISOString());
     } else if (Array.isArray(value)) {
       value.forEach((v, index) => {
-        if (typeof v === "object" && v !== null) {
-          formatFormData(v, formData, `${formKey}[${index}]`);
+        if (typeof v === 'object' && v !== null) {
+          obj2Form(v, formData, `${formKey}[${index}]`);
         } else {
           formData.append(`${formKey}[${index}]`, String(v));
         }
       });
-    } else if (typeof value === "object") {
-      formatFormData(value, formData, formKey);
+    } else if (typeof value === 'object') {
+      obj2Form(value, formData, formKey);
     } else {
       formData.append(formKey, String(value));
     }
