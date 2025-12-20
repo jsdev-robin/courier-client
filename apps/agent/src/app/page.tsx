64cee14e60155ba5590c393c2f885e3c0bacaa63
jsdev-robin <jsdev.robin@gmail.com>
2025-12-20 13:25:05 +0600

    feat: add parcel

--- apps/agent/src/app/page.tsx
+++ apps/agent/src/app/page.tsx
@@ -1,3 +1,35 @@
+// 'use client';
+
+// import { Redis } from '@upstash/redis';
+// import { useEffect } from 'react';
+
+// const redis = new Redis({
+//   url: 'https://proud-sunbeam-57313.upstash.io',
+//   token: 'key',
+// });
+
+// const Home = () => {
+//   useEffect(() => {
+//     const subscribeToMessages = async () => {
+//       const subscriber = redis.subscribe(['locationsChannel']);
+
+//       subscriber.on('message', (data) => {
+//         console.log(JSON.parse(data.message));
+//       });
+
+//       return () => {
+//         subscriber.unsubscribe();
+//       };
+//     };
+
+//     subscribeToMessages();
+//   }, []);
+
+//   return <div>d</div>;
+// };
+
+// export default Home;
+
 const page = () => {
   return <div>page</div>;
 };
